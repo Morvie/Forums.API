12b91f1ab62fbc855872eb97b28fffc48d46072b
Mark Goertz <58692211+markgoertz@users.noreply.github.com>
2022-11-21 18:46:53 +0100

    Added testing

---
+++ Forums.Test/Integration.Testing/ControllerTesting.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Azure;
+using ForumsService.Domain.Entities;
+using Xunit;
+
+namespace Forums.Test.Integration.Testing
+{
+    public class ControllerTesting
+    {
+
+        private readonly HttpClient _httpClient = new() { BaseAddress = new Uri("http://localhost:5000") };
+        [Fact]
+        public async Task DefaultRoute_ReturnsForumsList()
+        {
+            //Arrange
+            var expectedStatusCode = HttpStatusCode.OK;
+            var response = await _httpClient.GetAsync("");
+
+            //Act
+            var stringResult = await response.Content.ReadAsStringAsync();
+            var intResult = int.Parse(stringResult);
+
+            //Assert
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+            Assert.Equal(2, intResult);
+        }
+    }
+}
